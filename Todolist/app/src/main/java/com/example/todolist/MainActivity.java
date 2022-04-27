3f9bb14ddedad1d08007645ffd7ae07743f662ef
WIIASD <liziyang486@gmail.com>
2022-04-26 20:46:24 -0400

    todolist: more ui updates

--- Todolist/app/src/main/java/com/example/todolist/MainActivity.java
+++ Todolist/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
-import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<TodoItem> todoItems;
+    Button addButton, deleteButton;
+    TextInputLayout todoListItemTextBoxLayout;
+    TextInputEditText todoListItemTextBox;
+
     TDI_RecyclerViewAdapter adapter;
     LinearLayoutManager layoutManager;
 
@@ -20,51 +29,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initTodoItems();
+        addButton = findViewById(R.id.btnAddButton);
+        deleteButton = findViewById(R.id.btnDeleteButton);
+        todoListItemTextBox = findViewById(R.id.tietTodoItemEditText);
+        todoListItemTextBoxLayout = findViewById(R.id.tilTodoItemEditTextLayout);
+
+        fillTodoItems();
+
         adapter = new TDI_RecyclerViewAdapter(todoItems);
         layoutManager = new LinearLayoutManager(this);
 
         initAddButton();
         initDeleteButton();
-//        initTodoItemSelectedCheckbox(adapter);
+        initTodoListItemTextBox();
 
         setupAdapter();
     }
 
-//    private void initTodoItemSelectedCheckbox(TDI_RecyclerViewAdapter adapter) {
-//        CheckBox TodoItemSeleted = findViewById(R.id.todo_item_seleted);
-//        TodoItemSeleted.setOnClickListener(view -> {
-//            adapter.setSelected();
-//        });
-//    }
+    private void initTodoListItemTextBox() {
+        todoListItemTextBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(todoListItemTextBoxLayout.isErrorEnabled()){
+                    todoListItemTextBoxLayout.setErrorEnabled(false);
+                    todoListItemTextBoxLayout.setError(null);
+                }
+            }
+        });
+    }
 
     private void initAddButton() {
-        Button addButton =  findViewById(R.id.AddButton);
         addButton.setOnClickListener(view -> {
-            EditText s = findViewById(R.id.et_todo_item_string_enter);
-            TodoItem i = new TodoItem(s.getText().toString(), false);
+            String s = todoListItemTextBox.getText().toString();
+            if(s.equals("")) {
+                todoListItemTextBoxLayout.setErrorEnabled(true);
+                todoListItemTextBoxLayout.setError("Please Enter Something");
+                return;
+            }
+            todoListItemTextBoxLayout.setErrorEnabled(false);
+            todoListItemTextBoxLayout.setError(null);
+            TodoItem i = new TodoItem(s, false);
             adapter.addTodoItem(i);
-            s.getText().clear();
+            todoListItemTextBox.getText().clear();
         });
     }
 
     private void initDeleteButton() {
-        Button deleteButton = findViewById((R.id.DeleteButton));
         deleteButton.setOnClickListener(view -> {
             adapter.deleteCheckedItems();
         });
     }
 
-    private void initTodoItems(){
+    private void fillTodoItems(){
         todoItems =  new ArrayList<>();
-        TodoItem i1 = new TodoItem("Planetside is fun, play tonight at 9");
-        TodoItem i2 = new TodoItem("kasjdfklsfhjkashfsahflashfhnlasjhfasj");
+        TodoItem i1 = new TodoItem("🐱——————————");
+        TodoItem i2 = new TodoItem("Planetside is fun, play tonight at 9");
         todoItems.add(i1);
         todoItems.add(i2);
     }
 
     private void setupAdapter(){
-        RecyclerView recyclerView = findViewById(R.id.rv_todo_list);
+        RecyclerView recyclerView = findViewById(R.id.rvTodoList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
     }
